4c3e19b8e0f281928f65c8b1f96314039683fd8b
¨¨iCaaamz¨ <camalvaro15@gmail.com>
2022-05-28 00:04:47 -0500

    Add form

--- src/components/sections/Contac.js
+++ src/components/sections/Contac.js
@@ -3,11 +3,51 @@ import "./styles/contact.css"
  const Contact = () => {
      return(
          <>
-         <main>
-         <div>
-             <h1>MY SOCIAL MEDIA</h1>
+         <div className="containercontact">
+             <h1 className="h1contact">CONTACT TO <span>ME</span></h1>
+             <div className="contact-wrapper">
+                 <div className="contact-form">
+                     <h2>Alvaro Camacho</h2>
+                     <form action="">
+                         <p>
+                             <label for="fullname">FullName</label>
+                             <input type="text" name="FullName" id="fullname"/>
+                         </p>
+                         <p>
+                             <label for="email">Email</label>
+                             <input type="email" name="email" id="email"/> 
+                         </p>
+                         <p>
+                             <label for="affair">Affair</label>
+                             <input type="text" name="affair" id="affair"/>
+                         </p>
+                         <p>
+                             <label for="message">Message</label>
+                             <textarea name="message" id="message" rows="3"></textarea>
+                         </p>
+                         <p>
+                             <button type="submit">
+                                 Send
+                             </button>
+                         </p>
+                     </form>
+                 </div>
+                 <div className="contact-info">
+                     <h4>More info</h4>
+                     <ul>
+                         <li>Bogotá</li>
+                         <li>+57 3208849070</li>
+                         <li>camachodevelop@gmail.com</li>
+                     </ul>
+                     <p>
+                         Contact me through this form or directly by a message on my LinkedIn, I will read you as soon as possible
+                     </p>
+                     <p> 
+                         My LinkedIn <a href="https://www.linkedin.com/in/camacho-alvaro">Alvaro Camacho</a>
+                     </p>
+                 </div>
+             </div>
          </div>
-         </main>
          </>
      )
  }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import 'typeface-quicksand';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
